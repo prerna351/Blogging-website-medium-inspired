01e18f6a3cb2b8112fd964429f1c7b3b07d579ba
prerna351 <prernasharma7008@gmail.com>
2024-07-13 01:22:55 +0530

    change Backend url

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,1 @@
-// export const BACKEND_URL = "https://abc.prernasharma123.workers.dev"
-export const BACKEND_URL = "http://127.0.0.1:8787"
+export const BACKEND_URL = "https://medium.prernasharma123.workers.dev"
